099a90ec4ed2968d34c7db5eede2eebc3bf756a8
vyvienne <vivientan.wy@gmail.com>
2025-08-11 22:37:15 +0800

    allow side bar to shrink when in mobile mode, removed fixed length side bar

--- app/(main)/patients/layout.tsx
+++ app/(main)/patients/layout.tsx
@@ -1,17 +1,27 @@
 import { PatientSidebar } from "@/components/custom-ui/patient-side-bar";
 import { PatientTopNavBar } from "@/components/custom-ui/patient-top-nav-bar";
 
-
 export default function PatientLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex">
+        <div className="flex flex-col min-h-screen">
             <PatientTopNavBar />
-            <PatientSidebar />
-            <main className="flex-1 ml-64 p-6">{children}</main>
+            <div className="flex flex-1">
+                <main
+                    className="
+                        flex-1
+                        p-4 md:p-6
+                        transition-all duration-300 ease-in-out
+                        md:ml-64
+                    "
+                >
+                    <PatientSidebar />
+                    {children}
+                </main>
+            </div>
         </div>
     );
 }
